b57d605c5e91dad6884bad98b208cfa5b22a6354
kinndohyun <dohyun.kim@carbonsaurus.com>
2024-05-11 16:19:01 +0900

    [main] Update: eslint, vscode setting 추가

---
+++ tests/index.test.ts
@@ -0,0 +1,16 @@
+import { Page, test } from "@playwright/test";
+
+test.describe("initial page", () => {
+    let page: Page;
+
+    test.beforeAll(async ({ browser, contextOptions }) => {
+        const browserContext = await browser.newContext(contextOptions);
+        page = await browserContext.newPage();
+
+        await page.goto("http://localhost:3000");
+    });
+
+    test("has text", async () => {
+        await page.getByText("Get started by editing app/page.tsx");
+    });
+});
