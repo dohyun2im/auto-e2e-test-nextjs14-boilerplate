e5ac9889b6097b26abcca6d94cc72d0a5d5e95ab
kinndohyun <dohyun.kim@carbonsaurus.com>
2024-05-11 17:58:43 +0900

    [main] Config: playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  timeout: 60 * 1000 * 3,
+  timeout: 60 * 1000,
   use: {
     baseURL: "http://localhost:3000/",
     headless: true,
